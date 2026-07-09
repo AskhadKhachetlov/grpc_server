88eb0e6845ac98eb8ec8af892c16bff3b6fa03d4
Askhad Khachetlov <askhad.khachetlov@axxonsoft.dev>
2026-07-09 19:16:16 +0300

    Unify library naming

--- cmake/project_configuration.cmake
+++ cmake/project_configuration.cmake
@@ -1,15 +1,15 @@
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_EXPORT_COMPILE_COMMANDS OFF)
 
-add_library(ProjectConfiguration INTERFACE)
+add_library(project_configuration INTERFACE)
 
-target_compile_options(ProjectConfiguration
+target_compile_options(project_configuration
     INTERFACE
         "$<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/W4;/WX;/wd4702>"
         "$<$<NOT:$<COMPILE_LANG_AND_ID:CXX,MSVC>>:-Wall;-Wextra;-Wpedantic;-Werror>"
 )
 
-target_compile_features(ProjectConfiguration
+target_compile_features(project_configuration
         INTERFACE
             cxx_std_20
 )
